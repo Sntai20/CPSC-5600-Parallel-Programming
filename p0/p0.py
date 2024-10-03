bde620bdcfe742cffe85aac2dec09101b06bc383
Antonio Santana <47947504+Sntai20@users.noreply.github.com>
2024-10-03 02:00:41 +0000

    Refactor main program to use multithreading for parallel execution

---
+++ p0/p0.py
@@ -0,0 +1,15 @@
+import subprocess
+import math
+
+add_counts = [ 10, 20, 40, 80, 160, 320, 640 ]
+mul_counts = [ 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024 ]
+
+for i in add_counts:
+    for j in mul_counts:
+        cmd = [ "./p0", str(i), str(j) ]
+        output_str = subprocess \
+            .run(cmd,stdout=subprocess.PIPE) \
+            .stdout.decode('utf-8')
+        time = float(output_str)
+        print(f"{time},",end="")
+    print()
