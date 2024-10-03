987054bf859cb5a522bb06ee4141ab265980083d
Antonio Santana <47947504+Sntai20@users.noreply.github.com>
2024-10-03 02:00:33 +0000

    Refactor main program to use multithreading for parallel execution

--- p0/p0.cpp
+++ p0/p0.cpp
@@ -50,6 +50,15 @@ int *random_array(int count) {
     return result;
 }
 
+// class Worker {
+// public:
+//     void operator()(int id, void *sharedData) {
+//         std::cout << "Thread " << id << " is processing" << std::endl;
+//         std::string *data = static_cast<std::string *>(sharedData);
+//         std::cout << "Thread " << id << " received data: " << *data << std::endl;
+//     }
+// }
+
 // desc: Calculates the element-wise multiplication and
 //       addition of three arrays (A * B + C)
 //  pre: Command-line arguments should consist of exactly
@@ -62,21 +71,29 @@ int main(int argc, char *argv[]) {
     // - #pragma omp parallel for is the directive that tells OpenMP to parallelize the following `for` loop.
     // - omp_get_thread_num() returns the ID of the thread executing the current iteration of the loop.
 
-    // Set the number of threads
-    omp_set_num_threads(4);
+    // const int THREAD_COUNT = 4;
 
     int val_count, thread_count;
     get_args(argc, argv, val_count, thread_count);
 
-    std::cout << "Value count is  : " << val_count << '\n';
-    std::cout << "Thread count is : " << thread_count << '\n';
+    // Set the number of threads
+    omp_set_num_threads(thread_count);
+    // std::cout << "Value count is  : " << val_count << '\n';
+    // std::cout << "Thread count is : " << thread_count << '\n';
 
     int *a = random_array(val_count);
     int *b = random_array(val_count);
     int *c = random_array(val_count);
-
-    for (int i = 0; i < val_count; i++) {
+    
+    // Refactor this loop  so that it performs its primary loop (with C[i] = A[i] * B[i] + C[i]) using multiple threads.
+    // The number of threads should be set to the value of the second command-line argument.
+    // The number of iterations should be set to the value of the first command-line argument.
+    // The loop should be parallelized using OpenMP.
+    // #pragma omp parallel for
+    for (int i = 0; i < thread_count; i++) {
         c[i] = a[i] * b[i] + c[i];
+        // std::cout << "Thread " << i << " is processing iteration " << i << std::endl;
+        std::cout << c[i] << std::endl;
     }
 
     // Parallelize this loop
