d84eaa2267683a42fea65942168463a025600e90
Antonio Santana <47947504+Sntai20@users.noreply.github.com>
2024-10-03 00:28:15 +0000

    Add multithreading to main program

---
+++ lecture2/threads.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <string>
+
+void printy(std::string name) {
+    for (int i = 0; i < 5; i++) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        std::cout << "Hello from " << name << std::endl;
+    }
+}
+
+int main() {
+    std::thread t1(printy, "thread1");
+    std::thread t2(printy, "thread2");
+
+    t1.join();
+    t2.join();
+
+    return 0;
+}
