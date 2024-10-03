705c54fc54b70bdcb16ecdb65248ef08f5cab6ad
Antonio Santana <47947504+Sntai20@users.noreply.github.com>
2024-10-03 02:27:19 +0000

    Refactor main program to use multithreading for parallel execution and measure execution time

--- p0/p0.py
+++ p0/p0.py
@@ -4,12 +4,17 @@ import math
 add_counts = [ 10, 20, 40, 80, 160, 320, 640 ]
 mul_counts = [ 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024 ]
 
+sample_count = 10
+
 for i in add_counts:
     for j in mul_counts:
-        cmd = [ "./p0", str(i), str(j) ]
-        output_str = subprocess \
-            .run(cmd,stdout=subprocess.PIPE) \
-            .stdout.decode('utf-8')
-        time = float(output_str)
-        print(f"{time},",end="")
+        minimum = math.inf
+        for k in range(sample_count):
+            cmd = [ "./p0", str(i), str(j) ]
+            output_str = subprocess \
+                .run(cmd,stdout=subprocess.PIPE) \
+                .stdout.decode('utf-8')
+            time = float(output_str)
+            minimum = min(minimum,time)
+        print(f"{minimum},",end="")
     print()
