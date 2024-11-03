eff9a80f23bc97990a8a612ec7033fd6a214e919
Antonio Santana <asantana1@seattleu.edu>
2024-11-03 12:21:48 -0800

    Implement custom SharedPointer and AtomicIterator classes with basic functionality

--- p4/p4.cpp
+++ p4/p4.cpp
@@ -3,76 +3,98 @@
 #include <thread>
 #include <random>
 #include <chrono>
+#include <memory>
 
-// template<typename T>
-// class SharedPointer {
+#ifdef CUSTOM_SHAREDPOINTER
+template<typename T>
+class SharedPointer {
 
-//     public:
+    public:
 
-//     SharedPointer() {
-//         // ???
-//     }
+    SharedPointer() {
+        // Default constructs the pointer as null.
+    }
 
-//     SharedPointer(T* raw_pointer) {
-//         // ???
-//     }
+    SharedPointer(T* raw_pointer) {
+        // Captures a regular pointer and begins reference counting for it.
+    }
 
-//     SharedPointer(SharedPointer& other) {
-//         // ???
-//     }
+    SharedPointer(SharedPointer& other) {
+        // Copies the assigned SharedPointer into the constructing instance.
+    }
 
-//     ~SharedPointer() {
-//         // ???
-//     }
+    ~SharedPointer() {
+        // Destructor.
+    }
 
-//     SharedPointer& operator=(SharedPointer& other) {
-//         // ???
-//     }
+    SharedPointer& operator=(SharedPointer& other) {
+        // Overwrites the assigned SharedPointer with the assigning SharedPointer.
+    }
 
-//     T& operator*() {
-//         // ???
-//     }
+    T& operator*() {
+        // Returns a reference to the storage pointed by the wrapped pointer.
+    }
 
-//     T* operator->() {
-//         // ???
-//     }
+    T* operator->() {
+        // Returns the wrapped pointer.
+    }
 
-//     operator bool() {
-//         // ???
-//     }
+    operator bool() {
+        // Returns true if the wrapped pointer is not null.
+        return true;
+    }
 
-// };
+};
 
+#else
+template<typename T>
+using SharedPointer = std::shared_ptr<T>;
+#endif
 
+#ifdef CUSTOM_ATOMICITERATOR
+template<typename T>
+class AtomicIterator {
 
-// template<typename T>
-// class AtomicIterator {
+    public:
 
-//     public:
+    AtomicIterator(T* buffer, size_t size){
+        // ???
+    }
 
-//     AtomicIterator(T* buffer, size_t size){
-//         // ???
-//     }
+    AtomicIterator(AtomicIterator& other) {
+        // ???
+    }
 
-//     AtomicIterator(AtomicIterator& other) {
-//         // ???
-//     }
+    AtomicIterator& operator=(AtomicIterator& other) {
+        // ???
+    }
 
-//     AtomicIterator& operator=(AtomicIterator& other) {
-//         // ???
-//     }
+    T* next() {
+        // ???
+    }
 
-//     T* next() {
-//         // ???
-//     }
+};
 
-// };
+#else
+template<typename T>
+using AtomicIterator = std::atomic<T>;
+#endif
 
-
+struct MyClass {
+    MyClass() { std::cout << "MyClass created\n"; }
+    ~MyClass() { std::cout << "MyClass destroyed\n"; }
+};
 
 int main() {
+    std::cout << "Hello, World!" << std::endl;
 
     // You may call the test drivers for your implementations here
-    std::cout << "Hello, World!" << std::endl;
+    SharedPointer<MyClass> ptr1 = SharedPointer<MyClass>(new MyClass());
+    {
+        SharedPointer<MyClass> ptr2 = ptr1; // Shared ownership
+        std::cout << "ptr2 is sharing ownership with ptr1\n";
+    } // ptr2 goes out of scope, but the object is not destroyed because ptr1 still owns it
+
+    std::cout << "ptr1 is the last owner\n";
     return 0;
 }
