34da1cd1e89962daf2ddf23ffc687d38cf816e26
Antonio Santana <asantana1@seattleu.edu>
2024-12-07 12:25:47 -0800

    Refactor bitonic sort implementation and integrate into main program

--- p6/p6.cu
+++ p6/p6.cu
@@ -2,11 +2,14 @@
 #include <iostream>
 #include <string>
 
-using std::string;
+#include "bitonic_naive.cu"
+#include "reduce_scan_1block.cu"
+
+using namespace std;
 
 // Global Variables
 
-// Constant all CUDA blocks have 1024 threads.
+// True for all CUDA architectures so far. This is the maximum number of threads per block.
 const int MAX_BLOCK_SIZE = 1024;
 const string filename = "x_y.csv";
 const string output_filename = "x_y_scan.csv";
@@ -24,7 +27,41 @@ int main() {
     */
 
     // Sorts this sequence o (x,y) pairs by their x-values, using a parallel bitonic sort in CUDA.
+    int n;
+	cout << "n = ? (must be power of 2): ";
+	cin >> n;
+	if (n > MAX_BLOCK_SIZE || pow(2,floor(log2(n))) != n) {
+		cerr << "n must be power of 2 and <= " << MAX_BLOCK_SIZE << endl;
+		return 1;
+	}
 
+	// use managed memory for the data array
+	float *data;
+	cudaMallocManaged(&data, n * sizeof(*data));
+
+	// fill it with random values
+	random_device r;
+	default_random_engine gen(r());
+	uniform_real_distribution<float> rand(-3.14, +3.14);
+	for (int i = 0; i < n; i++)
+		data[i] = rand(gen);
+
+	// sort it with naive bitonic sort
+	for (int k = 2; k <= n; k *= 2) {
+		// coming back to the host between values of k acts as a barrier
+		// note that in later hardware (compute capabilty >= 7.0), there is a cuda::barrier available
+		bitonic<<<1, MAX_BLOCK_SIZE>>>(data, k);
+	}
+	cudaDeviceSynchronize();
+
+	// print out results
+	for (int i = 0; i < n; i++)
+		if (i < 3 || i >= n - 3 || i % 100 == 0)
+			cout << data[i] << " ";
+		else
+			cout << ".";
+	cout << endl;
+    cudaFree(data);
 
     // Scans the y-values in the sorted sequence using a parallel prefix scan Links to an external site. in CUDA.
     
