7a980d43204d9f85b4f2f0d1dcb37cd1c9f7f31a
Antonio Santana <ansantan@outlook.com>
2024-12-07 22:41:43 -0800

    Fixed bitonic_naive_sort_test.

--- p6/p6_test.cu
+++ p6/p6_test.cu
@@ -17,7 +17,7 @@ bool file_utils_read_csv_test(string input_filename) {
 
     // Check if the data size is correct
     if (data.size() != 16) {
-        cout << "----file_utils_read_csv_test failed ---- Expected 16 rows, got " << data.size() << endl;
+        cout << "Test failed: file_utils_read_csv_test failed ---- Expected 16 rows, got " << data.size() << endl;
         return false;
     }
 
@@ -29,20 +29,53 @@ bool file_utils_read_csv_test(string input_filename) {
     //     return false;
     // }
 
-    cout << "----file_utils_read_csv_test passed" << endl;
+    cout << "Test passed: file_utils_read_csv_test works correctly." << endl;
     
     return true;
 }
 
-// bool bitonic_naive_sort_test() {
-//     cout << "Running bitonic_naive_sort_test" << endl;
+bool bitonic_naive_sort_test() {
+    cout << "Running bitonic_naive_sort_test" << endl;
     
-//     bitonic_naive_sort(256);
+    // Initialize a sample array of X_Y structures
+    std::vector<X_Y> data = {
+        {3.0f, 1.0f, 0},
+        {1.0f, 2.0f, 1},
+        {4.0f, 3.0f, 2},
+        {2.0f, 4.0f, 3}
+    };
 
-//     cout << "----bitonic_naive_sort_test passed" << endl;
+    int n = data.size();
+    X_Y *d_data;
+    cudaMalloc(&d_data, n * sizeof(X_Y));
+    cudaMemcpy(d_data, data.data(), n * sizeof(X_Y), cudaMemcpyHostToDevice);
+
+    // Call the bitonic_sort function
+    bitonic_sort(d_data, n);
+
+    // Copy sorted data back to host
+    cudaMemcpy(data.data(), d_data, n * sizeof(X_Y), cudaMemcpyDeviceToHost);
+    cudaFree(d_data);
+
+    // Check if the data is sorted correctly
+    bool sorted = true;
+    for (int i = 1; i < n; ++i) {
+        if (data[i - 1].x > data[i].x) {
+            sorted = false;
+            break;
+        }
+    }
+
+    if (sorted) {
+        cout << "Test passed: bitonic_sort function works correctly." << endl;
+    } else {
+        cout << "Test failed: Data is not sorted correctly." << endl;
+    }
+
+    // cout << "----bitonic_naive_sort_test passed" << endl;
     
-//     return true;
-// }
+    return true;
+}
 
 // bool reduce_scan_1block_test() {
 //     cout << "Running reduce_scan_1block_test" << endl;
@@ -86,9 +119,9 @@ bool p6_test() {
         return false;
     }
 
-    // if (!bitonic_naive_sort_test()) {
-    //     return false;
-    // }
+    if (!bitonic_naive_sort_test()) {
+        return false;
+    }
 
     // if (!reduce_scan_1block_test()) {
     //     return false;
