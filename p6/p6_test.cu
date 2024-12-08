65ac1fb441cd0008ffb4f582e37d48fc6ab06996
Antonio Santana <ansantan@outlook.com>
2024-12-07 17:30:23 -0800

    Update dir paths

--- p6/p6_test.cu
+++ p6/p6_test.cu
@@ -6,8 +6,8 @@
 
 using namespace std;
 
-const string input_filename_test = "p6/out/test/x_y_16.csv";
-const string output_filename_test = "p6/out/test/x_y_scan_16.csv";
+const string input_filename_test = "out/test/x_y_16.csv";
+const string output_filename_test = "out/test/x_y_scan_16.csv";
 
 bool file_utils_read_csv_test(string input_filename) {
     cout << "Running file_utils_read_csv_test" << endl;
@@ -15,11 +15,11 @@ bool file_utils_read_csv_test(string input_filename) {
     std::vector<std::pair<float, float>> data = read_csv(input_filename);
 
     if (data.size() != 16) {
-        cout << "Expected 16 rows, got " << data.size() << endl;
+        cout << "----file_utils_read_csv_test failed ---- Expected 16 rows, got " << data.size() << endl;
         return false;
     }
 
-    cout << "file_utils_read_csv_test passed" << endl;
+    cout << "----file_utils_read_csv_test passed" << endl;
     
     return true;
 }
@@ -29,7 +29,7 @@ bool bitonic_naive_sort_test() {
     
     bitonic_naive_sort(256);
 
-    cout << "bitonic_naive_sort_test passed" << endl;
+    cout << "----bitonic_naive_sort_test passed" << endl;
     
     return true;
 }
@@ -39,12 +39,28 @@ bool reduce_scan_1block_test() {
     
     reduce_scan_1block();
 
-    cout << "reduce_scan_1block_test passed" << endl;
+    cout << "----reduce_scan_1block_test passed" << endl;
+    
+    return true;
+}
+
+bool file_utils_write_csv_test(string output_filename) {
+    cout << "Running file_utils_write_csv_test" << endl;
+    
+    std::vector<std::pair<float, float>> data = read_csv(output_filename);
+
+    if (data.size() != 16) {
+        cout << "----file_utils_write_csv_test failed ---- Expected 16 rows, got " << data.size() << endl;
+        return false;
+    }
+
+    cout << "----file_utils_write_csv_test passed" << endl;
     
     return true;
 }
 
 bool p6_test() {
+    cout << "\n" << endl;
     cout << "Running in test mode" << endl;
     cout << "Current working directory: " << get_current_directory() << endl;
 
@@ -54,6 +70,8 @@ bool p6_test() {
     string output_filename = output_filename_test;
     cout << "Writing to: " << output_filename << "\n\n" << endl;
     
+    cout << "\n" << endl;
+    cout << "Running tests" << endl;
     if (!file_utils_read_csv_test(input_filename_test)) {
         return false;
     }
@@ -65,6 +83,12 @@ bool p6_test() {
     if (!reduce_scan_1block_test()) {
         return false;
     }
+
+    if (!file_utils_write_csv_test(output_filename_test)) {
+        return false;
+    }
+
+    cout << "\n" << endl;
 
     return true;
 }
