85870485a6a471850cf2e2c38378bf80d1509411
Antonio Santana <ansantan@outlook.com>
2024-12-07 22:27:09 -0800

    commented out tests.

--- p6/p6_test.cu
+++ p6/p6_test.cu
@@ -9,55 +9,55 @@ using namespace std;
 const string input_filename_test = "out/test/x_y_16.csv";
 const string output_filename_test = "out/test/x_y_scan_16.csv";
 
-bool file_utils_read_csv_test(string input_filename) {
-    cout << "Running file_utils_read_csv_test" << endl;
+// bool file_utils_read_csv_test(string input_filename) {
+//     cout << "Running file_utils_read_csv_test" << endl;
     
-    std::vector<std::pair<float, float>> data = read_csv(input_filename);
+//     std::vector<std::pair<float, float>> data = read_csv(input_filename);
 
-    if (data.size() != 16) {
-        cout << "----file_utils_read_csv_test failed ---- Expected 16 rows, got " << data.size() << endl;
-        return false;
-    }
+//     if (data.size() != 16) {
+//         cout << "----file_utils_read_csv_test failed ---- Expected 16 rows, got " << data.size() << endl;
+//         return false;
+//     }
 
-    cout << "----file_utils_read_csv_test passed" << endl;
+//     cout << "----file_utils_read_csv_test passed" << endl;
     
-    return true;
-}
+//     return true;
+// }
 
-bool bitonic_naive_sort_test() {
-    cout << "Running bitonic_naive_sort_test" << endl;
+// bool bitonic_naive_sort_test() {
+//     cout << "Running bitonic_naive_sort_test" << endl;
     
-    bitonic_naive_sort(256);
+//     bitonic_naive_sort(256);
 
-    cout << "----bitonic_naive_sort_test passed" << endl;
+//     cout << "----bitonic_naive_sort_test passed" << endl;
     
-    return true;
-}
+//     return true;
+// }
 
-bool reduce_scan_1block_test() {
-    cout << "Running reduce_scan_1block_test" << endl;
+// bool reduce_scan_1block_test() {
+//     cout << "Running reduce_scan_1block_test" << endl;
     
-    reduce_scan_1block();
+//     reduce_scan_1block();
 
-    cout << "----reduce_scan_1block_test passed" << endl;
+//     cout << "----reduce_scan_1block_test passed" << endl;
     
-    return true;
-}
+//     return true;
+// }
 
-bool file_utils_write_csv_test(string output_filename) {
-    cout << "Running file_utils_write_csv_test" << endl;
+// bool file_utils_write_csv_test(string output_filename) {
+//     cout << "Running file_utils_write_csv_test" << endl;
     
-    std::vector<std::pair<float, float>> data = read_csv(output_filename);
+//     std::vector<std::pair<float, float>> data = read_csv(output_filename);
 
-    if (data.size() != 16) {
-        cout << "----file_utils_write_csv_test failed ---- Expected 16 rows, got " << data.size() << endl;
-        return false;
-    }
+//     if (data.size() != 16) {
+//         cout << "----file_utils_write_csv_test failed ---- Expected 16 rows, got " << data.size() << endl;
+//         return false;
+//     }
 
-    cout << "----file_utils_write_csv_test passed" << endl;
+//     cout << "----file_utils_write_csv_test passed" << endl;
     
-    return true;
-}
+//     return true;
+// }
 
 bool p6_test() {
     cout << "\n" << endl;
@@ -72,21 +72,21 @@ bool p6_test() {
     
     cout << "\n" << endl;
     cout << "Running tests" << endl;
-    if (!file_utils_read_csv_test(input_filename_test)) {
-        return false;
-    }
+    // if (!file_utils_read_csv_test(input_filename_test)) {
+    //     return false;
+    // }
 
-    if (!bitonic_naive_sort_test()) {
-        return false;
-    }
+    // if (!bitonic_naive_sort_test()) {
+    //     return false;
+    // }
 
-    if (!reduce_scan_1block_test()) {
-        return false;
-    }
+    // if (!reduce_scan_1block_test()) {
+    //     return false;
+    // }
 
-    if (!file_utils_write_csv_test(output_filename_test)) {
-        return false;
-    }
+    // if (!file_utils_write_csv_test(output_filename_test)) {
+    //     return false;
+    // }
 
     cout << "\n" << endl;
 
